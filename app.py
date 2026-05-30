56ffb0fd93035040e50623c23ab1bbf3e12b8e32
adithyanpm87-eng <adithyanpm87@gmail.com>
2026-05-30 11:28:27 +0530

    Update Flask host

--- app.py
+++ app.py
@@ -162,5 +162,5 @@ def history():
    
 
     if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5000, debug=True)
        app.run(debug=True)
